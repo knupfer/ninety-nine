2f5af5c2a3612c92f2d9e093ec332d8996f4f155
knupfer <fknupfer@gmail.com>
2015-04-28 23:37:53 +0200

    solve p2

--- src/Problem.hs
+++ src/Problem.hs
@@ -7,3 +7,11 @@ prop_problem1 xs = not (null xs) ==> head (reverse xs) == last xs
 
 problem1 :: [a] -> a
 problem1 = last
+
+prop_problem2 :: [Int] -> Property
+prop_problem2 xs = length xs > 1 ==> Just (last $ init xs) == problem2 xs
+
+problem2 :: [a] -> Maybe a
+problem2 xs
+          | length xs > 1 = Just . head . drop 1 $ reverse xs
+          | otherwise     = Nothing
