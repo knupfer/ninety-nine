4ec4a52e88e05fc678f3d1a891f46a5cac5271a4
knupfer <fknupfer@gmail.com>
2015-05-03 18:18:44 +0200

    add probs 7-8

--- src/Main.hs
+++ src/Main.hs
@@ -3,20 +3,25 @@ module Main where
 import Test.Invariant
 import Test.Tasty
 import Test.Tasty.QuickCheck
+import Data.List
 
 main :: IO ()
 main = defaultMain $
   testGroup "Tests"
     [ testProperty "Problem 1"   $
-      (>1) . length &> problem1 <~~ (tail :: [Int] -> [Int])
+                  (>1) . length &> problem1 <~~ (tail :: [Int] -> [Int])
     , testProperty "Problem 2"   $
-      (>2) . length &> problem2 <~~ (tail :: [Int] -> [Int])
+                  (>2) . length &> problem2 <~~ (tail :: [Int] -> [Int])
     , testProperty "Problem 3"     prop_problem3
     , testProperty "Problem 4"   $ problem4 <~~ (reverse :: [Int] -> [Int])
-    , testProperty "Problem 5.1" $ involutory (problem5 :: [Int] -> [Int])
-    , testProperty "Problem 5.2" $ length <~~ (problem5 :: [Int] -> [Int])
-    , testProperty "Problem 5.3" $ reverse @~> (problem5 :: [Int] -> [Int])
+    , testProperty "Problem 5.1" $ involutory   (problem5 :: [Int] -> [Int])
+    , testProperty "Problem 5.2" $ length   <~~ (problem5 :: [Int] -> [Int])
+    , testProperty "Problem 5.3" $ reverse  @~> (problem5 :: [Int] -> [Int])
     , testProperty "Problem 6"   $ problem6 <~~ (reverse :: [Int] -> [Int])
+    , testProperty "Problem 8.1" $ deflating (problem8 :: [Int] -> [Int])
+    , testProperty "Problem 8.2" $ reverse . problem8 <=> problem8 . (reverse :: [Int] -> [Int])
+    , testProperty "Problem 8.3" $ idempotent (problem8 :: [Int] -> [Int])
+    , testProperty "Problem 8.4" $ problem8 <=> (problem8' :: [Int] -> [Int])
     ]
 
 problem1 :: [a] -> a
@@ -52,3 +57,15 @@ problem5 _ = []
 problem6 :: Eq a => [a] -> Bool
 problem6 = reverse <=> id
 
+data Lisp a = Leaf a | Branch [Lisp a]
+
+problem7 :: Lisp a -> [a]
+problem7 (Leaf a) = [a]
+problem7 (Branch xs) = concatMap problem7 xs
+
+problem8 :: Eq a => [a] -> [a]
+problem8 (x:xs) = x: problem8 (dropWhile (==x) xs)
+problem8 [] = []
+
+problem8' :: Eq a => [a] -> [a]
+problem8' = map head . group
